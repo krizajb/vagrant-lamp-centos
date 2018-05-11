eb3e2fce7251e0bed8836e6e73254af6be21a73d
Blaž Križaj <blaz.krizaj@gmail.com>
2018-05-11 15:50:38 +0200

    php-intl module added to the provisioning.

--- provision/php.sh
+++ provision/php.sh
@@ -3,7 +3,7 @@
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum-config-manager --enable remi-php72
-yum -y install php php-soap php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
+yum -y install php php-soap php_intl php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
 
 echo "configuring php"
 PHP_MEMORY_LIMIT=256M
