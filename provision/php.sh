d9b893ef67505722462a39fb28867686f221936a
Blaž Križaj <blaz.krizaj@gmail.com>
2018-05-11 15:47:42 +0200

    Setting php memory limit to 256 Mb.

--- provision/php.sh
+++ provision/php.sh
@@ -4,3 +4,7 @@ yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarc
 yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum-config-manager --enable remi-php72
 yum -y install php php-soap php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
+
+echo "configuring php"
+PHP_MEMORY_LIMIT=256M
+sed -i 's/memory_limit = .*/memory_limit = '${PHP_MEMORY_LIMIT}'/' /etc/php.ini
